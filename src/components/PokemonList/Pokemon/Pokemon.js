464100ae0cfb332541300559f902672219de24f8
AndroShivam <shivi08567@gmail.com>
2021-06-06 11:10:14 +0530

    Added scroll to top button and fixed input styling

--- src/components/PokemonList/Pokemon/Pokemon.js
+++ src/components/PokemonList/Pokemon/Pokemon.js
@@ -7,6 +7,8 @@ const Pokemon = ({ pokemons, gotoNextPage }) => {
 
     const checkHasMore = () => pokemons.length < 1100
 
+    const scrollToTop = () => window.scrollTo(0,0)
+
     if(pokemons.length === 1){
         return <Card pokemons={pokemons}/>
     }else {
@@ -19,6 +21,7 @@ const Pokemon = ({ pokemons, gotoNextPage }) => {
                 <div className="load-container">
                     <img className="load-img" src = "https://i.imgur.com/aMz1Qtu.gif" alt="loading" />
                 </div>}>
+                <img className = "scroll-top" onClick={() => scrollToTop()} src="https://img.icons8.com/windows/64/000000/circled-up-2.png"/>
                 <Card pokemons={pokemons}/>        
             </InfiniteScroll>
         )
